1062b2ac7be92b802d2cf4f4215849a30d9fa4c5
ks563 <karen.shea90@gmail.com>
2019-01-23 21:26:19 -0600

    html set up, js firebase

---
+++ assets/javascript/schedule.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyCYV-CRYXayHLEX2xF76YGG9iLQeZvKPhs",
+    authDomain: "train-scheduler-ks.firebaseapp.com",
+    databaseURL: "https://train-scheduler-ks.firebaseio.com",
+    projectId: "train-scheduler-ks",
+    storageBucket: "train-scheduler-ks.appspot.com",
+    messagingSenderId: "625011446425"
+  };
+firebase.initializeApp(config);
+  
+var database = firebase.database();
