2926fd3a4fcd2db5bfd5b0009ad360d08fad757e
ks563 <karen.shea90@gmail.com>
2019-01-24 18:02:44 -0600

    trying to add to firebaes

--- assets/javascript/schedule.js
+++ assets/javascript/schedule.js
@@ -18,4 +18,28 @@ $("#add-train-btn").on("click", function () {
   var initialTrain = moment($("#depature-input").val().trim()).format(H);
   var trainFrequency = $("#frequency-input").val().trim();
 
+
+  var trainData = {
+    name: trainName,
+    location: destination,
+    initial: initialTrain,
+    frequency: trainFrequency
+  };
+
+  database.ref().push(trainData);
+
+  console.log(trainData.name);
+  console.log(trainData.location);
+  console.log(trainData.initial);
+  console.log(trainData.frequency);
+
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#depature-input").val("");
+  $("#frequency-input").val("");
+
 });
+
+var trainDisplay = $("$<tr>").append()
+
+//minutes till next arrival current time * frequency
